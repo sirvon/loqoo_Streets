a87c8b4b58bcb0746d7930ebb694ee8365f21202
sirvon <sirvon@loqoo.tv>
2013-02-15 22:04:28 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/R.java
+++ src/com/loqoo/streets/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int border=0x7f020000;
         public static final int borderl=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int washav540front=0x7f020003;
     }
     public static final class id {
+        public static final int 540=0x7f050004;
         public static final int custom=0x7f050002;
         public static final int fstwash=0x7f050003;
         public static final int scrollView1=0x7f050000;
-        public static final int scrollView2=0x7f050004;
+        public static final int scrollView2=0x7f050005;
         public static final int standard=0x7f050001;
-        public static final int webview=0x7f050005;
+        public static final int webview=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
