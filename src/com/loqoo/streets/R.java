f4ce4505a8101bf4e780a873f7009f0ff3f634b0
sirvon <sirvon@loqoo.tv>
2013-02-16 18:50:05 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/R.java
+++ src/com/loqoo/streets/R.java
@@ -24,14 +24,15 @@ public final class R {
     }
     public static final class id {
         public static final int custom=0x7f050002;
-        public static final int fivefiftyfive=0x7f050006;
-        public static final int fivefiftyfive1=0x7f050007;
+        public static final int fivefiftyfive=0x7f050007;
+        public static final int fivefiftyfive1=0x7f050008;
         public static final int fiveforty=0x7f050004;
         public static final int fstwash=0x7f050003;
         public static final int scrollView1=0x7f050000;
-        public static final int scrollView2=0x7f050005;
+        public static final int scrollView1a=0x7f050005;
+        public static final int scrollView2=0x7f050006;
         public static final int standard=0x7f050001;
-        public static final int webview=0x7f050008;
+        public static final int webview=0x7f050009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
