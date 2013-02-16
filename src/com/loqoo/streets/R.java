708d79dc20903d401f22eaf25e49c4b7af1145bd
sirvon <sirvon@loqoo.tv>
2013-02-16 20:34:55 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/R.java
+++ src/com/loqoo/streets/R.java
@@ -29,10 +29,11 @@ public final class R {
         public static final int fiveforty=0x7f050004;
         public static final int fstwash=0x7f050003;
         public static final int scrollView1=0x7f050000;
-        public static final int scrollView1a=0x7f050005;
         public static final int scrollView2=0x7f050006;
         public static final int standard=0x7f050001;
-        public static final int webview=0x7f050009;
+        public static final int washav_east_sidewalk=0x7f050009;
+        public static final int washav_west_sidewalk=0x7f050005;
+        public static final int webview=0x7f05000a;
     }
     public static final class layout {
         public static final int main=0x7f030000;
