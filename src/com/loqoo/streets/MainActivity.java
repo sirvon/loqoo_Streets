7e6ff0506f38d2e3432dbcd2800fc246c79a6354
sirvon <sirvon@loqoo.tv>
2013-02-17 07:12:29 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/MainActivity.java
+++ src/com/loqoo/streets/MainActivity.java
@@ -3,6 +3,10 @@ package com.loqoo.streets;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ScrollView;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends Activity implements ScrollViewListener {
 
@@ -15,6 +19,17 @@ public class MainActivity extends Activity implements ScrollViewListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button connect = (Button)findViewById(R.id.custom);
+
+        connect.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View view) {
+             Intent intent = new Intent(MainActivity.this, Connect.class);
+               startActivity(intent);
+           }
+        });
         
 
         scrollView1 = (ObservableScrollView) findViewById(R.id.washav_west_biz);
