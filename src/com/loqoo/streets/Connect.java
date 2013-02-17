99c3af91a9d035339d6219643bad212191973938
sirvon <sirvon@loqoo.tv>
2013-02-17 07:28:16 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/Connect.java
+++ src/com/loqoo/streets/Connect.java
@@ -1,12 +1,49 @@
-package com.loqoo.streets;
+package com.loqoo.streets
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+ 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.view.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.*;
 
-public class Connect extends Activity
-{
+ 
+    public String readJSONFeed(String URL) {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL);
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            } else {
+                Log.d("JSON", "Failed to download file");
+            }
+        } catch (Exception e) {
+            Log.d("readJSONFeed", e.getLocalizedMessage());
+        }        
+        return stringBuilder.toString();
+    }
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
