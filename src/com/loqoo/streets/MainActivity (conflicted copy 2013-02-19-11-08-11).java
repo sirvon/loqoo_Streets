741f9ff37e4741a274a1852bf2a97aa564e36628
sirvon <sirvon@loqoo.tv>
2013-02-20 04:08:20 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

---
+++ src/com/loqoo/streets/MainActivity (conflicted copy 2013-02-19-11-08-11).java
@@ -0,0 +1,23 @@
+package com.loqoo.streets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+  }
+
+}
+
