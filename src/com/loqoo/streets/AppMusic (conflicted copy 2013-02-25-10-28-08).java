6c03c02940d4bc5902d0037e8339bf382d98442b
sirvon <sirvon@loqoo.tv>
2013-02-26 03:28:16 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

---
+++ src/com/loqoo/streets/AppMusic (conflicted copy 2013-02-25-10-28-08).java
@@ -0,0 +1,78 @@
+package com.loqoo.streets;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.media.MediaPlayer;
+import android.content.Context;
+
+public class AppMusic extends Service {
+
+             public static boolean isRunning = false;
+             MediaPlayer player;
+
+             @Override
+                 public IBinder onBind(Intent arg0) {
+                            return null;
+                 }
+
+             @Override
+                 public void onCreate() {
+                            super.onCreate();
+                     setMusicOptions(this, AppEngine.LOOP_BACKGROUND_MUSIC,AppEngine.R_VOLUME, AppEngine.L_VOLUME, AppEngine.SPLASH_SCREEN_MUSIC);
+           }
+            public void setMusicOptions(Context context, boolean isLooped, int rVolume, int lVolume,
+ int soundFile){
+                             player = MediaPlayer.create(context, soundFile);
+                             player.setLooping(isLooped);
+                             player.setVolume(rVolume,lVolume);
+            }
+              
+
+                 public int onStartCommand(Intent intent, int flags, int startId) {
+                            try                    
+                     {
+                            player.start();
+                            isRunning = true;
+                                         }catch(Exception e){
+                            isRunning = false;
+                            player.stop();
+                                         }                                          
+                                         return 1;
+                     }
+
+
+                 public void onStart(Intent intent, int startId) {
+
+                 }
+
+                 public void onStop() {
+                           isRunning = false;
+  
+                 }
+
+                 public IBinder onUnBind(Intent arg0) {
+             // TODO Auto-generated method stub              
+                               return null;
+                 }
+
+                 public void onPause() {
+                            player.pause();
+                 }             
+
+                @Override
+                public void onDestroy() {
+                            player.stop();
+                            player.release();
+
+                 }                  
+
+                @Override
+                public void onLowMemory() {
+                            player.stop();
+                         
+
+                 }
+
+  } 
+
---
+++ src/com/loqoo/streets/MainActivity (conflicted copy 2013-02-25-10-28-08).java
@@ -0,0 +1,31 @@
+package com.loqoo.streets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        AppEngine.musicThread = new Thread() {
+                            public void run() {
+                                             Intent bgmusic = new Intent(getApplicationContext(), AppMusic.class);
+                                             startService(bgmusic);
+                                             AppEngine.context = getApplicationContext();
+                                             }
+                                            };
+        AppEngine.musicThread.start();
+
+  }
+
+}
+
