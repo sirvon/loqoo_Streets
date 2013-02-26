f74caa272d7dfa583d9ba6c4cfef6fe21e6e4fdd
sirvon <sirvon@loqoo.tv>
2013-02-26 05:14:30 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/MainActivity.java
+++ src/com/loqoo/streets/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
 
 @Override
   protected void onPause {
-                            public void run() {
+                            public void call() {
                                              Intent bgmusic = new Intent(getApplicationContext(), AppMusic.class);
                                              stopService(bgmusic);
                                              AppEngine.context = getApplicationContext();
