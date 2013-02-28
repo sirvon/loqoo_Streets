c6e9eb8ff8752edc8f2dd5fe60327630d23b2695
sirvon <sirvon@loqoo.tv>
2013-02-28 15:52:23 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

---
+++ src/com/loqoo/streets/LoqooTvList.java
@@ -0,0 +1,17 @@
+package com.loqoo.streets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+
+public class LoqooTvList extends Activity {
+
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loqootvlisting);
+
+}
+
+}
