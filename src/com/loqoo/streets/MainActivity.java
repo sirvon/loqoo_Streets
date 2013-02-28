59c77975549fe9f35dc1f4ede517e99be64b15bd
sirvon <sirvon@loqoo.tv>
2013-02-28 23:58:24 +0000

    some code in  this APP has been modified. check the diffs or email sirvonthomas@gmail.com immediately, for more info.

--- src/com/loqoo/streets/MainActivity.java
+++ src/com/loqoo/streets/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.content.Context;
-import android.media.AudioManager;
 
 public class MainActivity extends Activity {
 
